44cd93dbd8e8b75d842f0e75eb005caa89d18fc1
Farzad dh <f0rz0d@outlook.com>
2021-08-10 08:06:02 +0430

    refactor scheduler service

---
+++ Owleye/Service/QuartzBootStrap.cs
@@ -0,0 +1,24 @@
+﻿using Owleye.Model.Model;
+using Quartz.Impl;
+
+namespace Owleye.Service
+{
+    public partial class QuartzBootStrap
+    {
+        public void Boot()
+        {
+
+            var scheduler = StdSchedulerFactory.GetDefaultScheduler().Result; //TODO fix this.
+            scheduler.Start();
+
+            var schedulerService = ServiceLocator.Resolve<IQrtzSchedule>();
+
+            schedulerService.schedule<QuartzJob>(scheduler, SensorInterval.OneMinute);
+            schedulerService.schedule<QuartzJob>(scheduler, SensorInterval.ThirtySecond);
+            schedulerService.schedule<QuartzJob>(scheduler, SensorInterval.FifteenMinutes);
+            schedulerService.schedule<QuartzJob>(scheduler, SensorInterval.FiveMinute);
+
+        }
+    }
+
+}
